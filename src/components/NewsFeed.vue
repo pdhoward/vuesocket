b69b60302399c8e42298b3a54d3fef2d768fd82e
pdhoward <patrick.howard@hotmail.com>
2019-05-21 09:44:32 -0400

    fix lifecycle component

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -1,13 +1,13 @@
 <template>
-  <FrameLiveFeed
+  <FrameLiveFeed     
     v-slot="{ entities: news }"
-    endpoint="http://localhost:8200/news"
+    :endpoint="endpoint"
   >
     <ul >
       <li
         v-for="newsEntity in news"
         :key="newsEntity.data.id"
-      >        
+      >
         <ContentCard
           :component="newsEntity.component"
           :props="newsEntity.data"
@@ -26,7 +26,28 @@ export default {
   components: {   
     FrameLiveFeed,
     ContentCard
-  } 
+  },
+  data() {
+    return {
+      endpoint: "http://localhost:8200/news",
+      devendpoint: "http://localhost:8200/news",
+      prodendpoint: "https://chaptoc.ngrok.io/news"
+    }
+  },
+  methods: {
+    setEnv: function() {        
+      if (process.env.NODE_ENV === 'development') {
+        console.log(`DEVELOPMENT ENDPOINT SET`)
+        this.endpoint = this.devendpoint
+      } else {
+        console.log(`PRODUCTION ENDPOINT SET`)
+        this.endpoint = this.prodendpoint
+      }     
+    },
+  },
+  beforeMount(){
+    this.setEnv()
+  }
 };
 </script>
 
