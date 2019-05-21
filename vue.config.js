fe4ff4da02ba2db14c810ff77f51f1d7e37a1d64
pdhoward <patrick.howard@hotmail.com>
2019-05-21 11:01:22 -0400

    debugging netlify deploy

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,8 @@
-const nodeSassMagicImporter = require(`node-sass-magic-importer`);
 
 module.exports = {
   configureWebpack: {
     devServer: {
       clientLogLevel: `error`,
-    },
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        importer: nodeSassMagicImporter(),
-      },
     },
   },
   lintOnSave: false,
