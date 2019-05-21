4ccf03b56e5c189d57e310345551f8f735c2b360
pdhoward <patrick.howard@hotmail.com>
2019-05-21 13:29:35 -0400

    debugging netlify deploy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import App from './App.vue';
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+const requireComponent = require.context(  
+  './components',  
+  false,  
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {  
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(     
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+  
+  Vue.component(
+    componentName,    
+    componentConfig.default || componentConfig
+  )
+})
 
 new Vue({
   store,
