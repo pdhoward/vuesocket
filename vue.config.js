aae96cefb0b42146464d0e323d28c26371bdab40
pdhoward <patrick.howard@hotmail.com>
2019-05-22 10:16:29 -0400

    pwa updates

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,17 @@ module.exports = {
   pwa: {
     name: 'vuemachine',
     themeColor: '#3746e8',
+    msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
-    manifestPath: './src/manifest.json',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    },
+    manifestPath: './public/manifest.json',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
